df53620772c825bc869b79599dd880aceaf42dd7
Martin T <martinthenot@gmail.com>
2024-03-16 15:30:12 +0100

    feat: add homepage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,7 +25,7 @@ import { KindeProvider, useKindeAuth } from "@kinde-oss/kinde-auth-nextjs";
 import { redirect } from "next/navigation";
 import { SettingOutlined, UserOutlined } from "@ant-design/icons";
 import "dayjs/locale/fr";
-import Loader from "@components/Loader/Loader";
+import PageLoader from "@components/PageLoader";
 
 const client = (getToken: () => string | null) =>
   new GraphQLClient(process.env.NEXT_PUBLIC_API_URL!, {
@@ -62,7 +62,7 @@ const App = (props: React.PropsWithChildren) => {
   };
 
   if (isLoading) {
-    return <Loader />;
+    return <PageLoader />;
   }
 
   const authProvider: AuthBindings = {
